344ed01dd47d8d469f8d2decb6baf61792cb59dd
Tom Endejan <Tom.Endejan@gmail.com>
2025-09-08 17:58:17 -0400

    Add missing __init__.py to src directory to make it a proper Python package

---
+++ novel_toy_tools/src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Source modules for novel_toy_tools
+"""
+
+from .wavefront_novel_toy import WavefrontNovelToy
+from .opengl_renderer import ObjectRenderer
+
+__all__ = ['WavefrontNovelToy', 'ObjectRenderer']
